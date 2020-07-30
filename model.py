86b161d6f2e7ce228b69ed8bb01e2e1471cd5eef
mediatrang <55777769+mediatrang@users.noreply.github.com>
2020-07-30 10:59:36 +1000

    Create model.py

---
+++ model.py
@@ -0,0 +1,55 @@
+# Dense layers set
+def dense_set(inp_layer, n, activation, drop_rate=0.):
+    dp = Dropout(drop_rate)(inp_layer)
+    dns = Dense(n)(dp)
+    bn = BatchNormalization(axis=-1)(dns)
+    act = Activation(activation=activation)(bn)
+    return act
+
+# Conv. layers set
+def conv_layer(feature_batch, feature_map, kernel_size=(3, 3),strides=(1,1), zp_flag=False):
+    if zp_flag:
+        zp = ZeroPadding2D((1,1))(feature_batch)
+    else:
+        zp = feature_batch
+    conv = Conv2D(filters=feature_map, kernel_size=kernel_size, strides=strides)(zp)
+    bn = BatchNormalization(axis=3)(conv)
+    act = LeakyReLU(1/10)(bn)
+    return act
+
+
+def get_model():
+    inp_img = tf.keras.Input(shape=(64, 64, 3))
+
+  
+    conv1 = conv_layer(inp_img, 64, zp_flag=False)
+    conv2 = conv_layer(conv1, 64, zp_flag=False)
+    mp1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv2)
+   
+    conv3 = conv_layer(mp1, 128, zp_flag=False)
+    conv4 = conv_layer(conv3, 128, zp_flag=False)
+    mp2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv4)
+    
+    conv7 = conv_layer(mp2, 256, zp_flag=False)
+    conv8 = conv_layer(conv7, 256, zp_flag=False)
+    conv9 = conv_layer(conv8, 256, zp_flag=False)
+    mp3 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv9)
+    
+    # dense layers
+    flt = Flatten()(mp3)
+    ds1 = dense_set(flt, 128, activation='relu')
+    out = dense_set(ds1, 42, activation='softmax')
+
+    model = Model(inputs=inp_img, outputs=out)
+    
+    model.compile(loss='categorical_crossentropy',
+                   optimizer='adam',
+                   metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+def get_callbacks(filepath, patience=5):
+    lr_reduce = ReduceLROnPlateau(monitor='val_acc', factor=0.1, epsilon=1e-5, patience=patience, verbose=1)
+    msave = ModelCheckpoint(filepath, save_best_only=True)
+    return [lr_reduce, msave]
